41822bb67a1b31a1022e41f51c47421ce545a8b8
RossMeikleham <rossmeikleham@hotmail.co.uk>
2014-12-09 18:40:04 +0000

    Added matlab file for question 6

--- q5.m
+++ q5.m
@@ -1,12 +1,14 @@
-function res = bvp(mu)
 % Code to calculate the solution to a 
 % nonlinear BVP with Dirichlet and Robin BCs
+
+% Some physical parameter.
+mu = 10;
 
 % Define the interval over which solution is calculated.
 a= 0; b= 1;
 
 % Define N.
-N = 100;
+N= 250;
 
 % Define the grid spacing.
 h=(b-a)/N;
@@ -25,17 +27,19 @@ tol= 10^(-8);
 F=ones(N,1);
 J=zeros(N,N);
 
+% Store the initial guess in SOL.
+SOL= U;
+
 % Loop while the norm(F) is greater than tol.
 while (norm(F)>tol )
 
     %% Define F(u).
     % Boundary conditions.
-    F(N) = (U(N) * ((2 * h * (U(N)^2)) + 3)) - (4 * (U(N-1))) + U(N-2);
+     F(N) = (U(N) * ((2 * h * (U(N)^2)) + 3)) - (4 * (U(N-1))) + U(N-2);
 
     % Finite difference approximation to ODE at interior nodes.
     F(1) = (U(2) * (2 + h * exp(U(1)))) - (4 * U(1)) + (2 - (h * exp(U(1))));
     F(1) = F(1) - (2 * (h^2) * mu * sin(2 * pi * x(1)));
-
     for i=2:N-1
         F(i)= (U(i+1) * (2 + h * exp(U(i)))) - (4 * U(i)) + U(i-1)*(2 - h * exp(U(i)));
         F(i) = F(i) - (2 * (h^2) * mu * sin(2 * pi * x(i)));
@@ -46,6 +50,7 @@ while (norm(F)>tol )
     J(1,:) = horzcat(((U(2) - 1) * h * exp(U(1))) - 4, 2 + (h * exp(U(1))), zeros(1, N-2));
 
     % Last row corresponds to BC at x=1.
+    %J(N,:) = horzcat(zeros(1, N-2),  2/(h^2), -2/(h^2) - (U(N)^2) * exp(U(N))*(U(N) + 3));
     J(N,:) = horzcat(zeros(1, N-3), 1, -4, (4 * h * U(N)^2) + 3); 
 
     % Intermediate rows correspond to F(i)=...
@@ -59,12 +64,17 @@ while (norm(F)>tol )
     end
     
     %% Having defined F and J, update the approximate solution to
-    % the difference equations.  
+    % the difference equations.
+    
     U=U-J\F;
 
-end
-
-% Return values of U at X = 1/4, X = 1/2, and X = 3/4
-res = [U(N/4), U(N/2), U((3*N)/4)];
+    %% Store the new approximation.
+    SOL=[SOL,U];
 
 end
+
+%% Insert your plot commands here.
+%plot(x, SOL(1));
+SOL
+%sprintf('%0.8f\n',SOL)
+
